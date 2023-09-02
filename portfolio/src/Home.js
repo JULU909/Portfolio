9c8c64924c10bee34ff053130559b525d64dd41f
Harish <julu909@gmail.com>
2023-09-02 23:30:37 +0800

    refactored home page and navigation bar

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -3,24 +3,19 @@ import './Home.css'
 
 function Home() {
     return (
-    <div class="container">
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <p>Explore the future with us!</p>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-        <h1>Welcome to the Futuristic Home Page</h1>
-
-
-
-     </div>
-    );
-  }
+      <div className="home-container">
+      <div className="image-container">
+        <img src={process.env.PUBLIC_URL + '/headGreen.png'} alt="Your Image" />
+      </div>
+      <div className="description-container">
+        <h2>Hi, I am Harish Vasanth.</h2>
+        <p>
+            Machine Learning Engineer
+        </p>
+      </div>
+    </div>
+  )}
+  ;
   
   export default Home;
   
